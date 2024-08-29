a4898697dfa5fb102045b868acdc9d883f5c9b3a
namizz <139370392+namizz@users.noreply.github.com>
2024-08-29 00:23:57 -0700

    learn

---
+++ src/Boxescolor.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BoxesColor(props) {
+  return <div className="box"></div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import boxes from "./boxes";
+import "./index.css";
+import BoxesColor from "./Boxescolor";
 
+function App() {
+  const [square, setSquares] = React.useState(boxes);
+  const [on, setOn] = React.useState(boxes.on);
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+  const style = {
+    backgroundColor: on ? "#222222" : "transparent",
+  };
+  const squareElements = square.map((s) => <BoxesColor key={s.id} on={s.on} />);
+  return (
+    <div className="boxes" onClick={toggle} style={style}>
+      {squareElements}
+    </div>
+  );
+}
 ReactDOM.render(
-  React.createElement("h1", "null", "Hello World"),
+  <div>
+    <h1>Boxes will go here</h1>
+    <App />
+  </div>,
   document.getElementById("root")
 );
 
